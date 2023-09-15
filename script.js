57e4e387ce0300c626f606ed1a01c8615e1f218a
Emilly Emanuele Maciel Prieto <125365495+EmillyPrieto@users.noreply.github.com>
2023-09-15 09:49:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,61 @@ function exercicio1(){
   `
 
 }
+
+function exercicio2() {
+  numero = 10;
+  intervalo = setInterval(() => {
+    numero--
+    document.getElementById("root2").style.display = "block"
+    document.getElementById("root2").innerHTML = `<p>Ano Novo em: ${numero}</p>`;
+
+    if (numero === 0) {
+      fecharintervalo();
+      document.getElementById("root2").innerHTML = `<p>Feliz Ano Novo</p>`;
+    }
+  }, 
+}
+
+ function fecharintervalo() {
+    clearInterval(intervalo)
+  }
+
+function exercicio3() {
+  var AA = prompt("Nome");
+  var BB = prompt("Senha");
+
+  while (AA === BB) {
+    alert("Os campos estão iguais")
+    AA = prompt("Nome");
+    BB = prompt("Senha");
+  }
+
+  document.getElementById("root3").style.display = "block"
+  document.getElementById("root3").innerHTML = `${AA}${BB}`;
+
+}
+
+var exercicio4 = document.getElementById("Exercicio4")
+
+function showExercicio4() {
+  exercicio4.style.display = "flex"
+}
+
+function somaExercio4() {
+  var num1 = document.getElementById("num1").value
+  var num2 = document.getElementById("num2").value
+
+  var resultado = parseInt(num1) + parseInt(num2);
+
+  document.getElementById("root4").style.display = "block"
+  document.getElementById("root4").innerHTML = resultado;
+}
+
+function limpar() {
+  document.getElementById("root1").style.display = "none"
+  document.getElementById("root2").style.display = "none"
+  document.getElementById("root3").style.display = "none"
+  document.getElementById("root4").style.display = "none"
+  exercicio4 = document.getElementById("Exercicio4").style.display = "none"
+  fecharintervalo()
+}
